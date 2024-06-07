93c8a2e9f48199727e1351705f2a13deda48d833
Chulani Vimukthi <vimukthichulani@gmail.com>
2024-06-07 14:51:52 +0530

    update files - 2024/06/07

--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -2,35 +2,133 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Accordion from 'react-bootstrap/Accordion';
 import './SingleProduct.css';
 import Button from 'react-bootstrap/Button';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Form from "react-bootstrap/Form";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import { useState } from 'react';
 
 function SingleProduct() {
+
+    const breadcrumbitem = {
+        textDecoration: 'none',
+    };
+
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
   return (
     
     <Container className='mt-5'>
         <Breadcrumb className='mb-5'>
-            <Breadcrumb.Item href="#" className="breadcrumbitem">Home</Breadcrumb.Item>
-            <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+            <Breadcrumb.Item href="#" className="breadcrumbitem" style={breadcrumbitem}>Home</Breadcrumb.Item>
+            <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/" className="breadcrumbitem">
                 Products
             </Breadcrumb.Item>
-            <Breadcrumb.Item active>Single Product</Breadcrumb.Item>
+            <Breadcrumb.Item active className="breadcrumbitem">Single Product</Breadcrumb.Item>
         </Breadcrumb>
 
-       <Row className='d-flex justify-content-center align-items-center'>
-            <Col>
-                <Col lg="7" className="mb-3">
-                <iframe className='rounded' width="560" height="315" src="https://www.youtube.com/embed/FCe1vPuB954?si=jymXBiNeo0O8Sv4h" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                </Col>
+        <Row>
+            <Col lg='6'>
+            <>
+            <Swiper
+              style={{
+                '--swiper-navigation-color': '#fff',
+                '--swiper-pagination-color': '#fff',
+              }}
+              loop={true}
+              spaceBetween={10}
+              navigation={true}
+              thumbs={{ swiper: thumbsSwiper }}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className="mySwiper2"
+            >
+              <SwiperSlide>
+              <iframe className='youtubevideo' src="https://www.youtube.com/embed/FCe1vPuB954?si=Ji0lDq6o_STvlgCB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              </SwiperSlide>
+              <SwiperSlide>
+                <img className='sliderimage' src="https://swiperjs.com/demos/images/nature-2.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img className='sliderimage' src="https://swiperjs.com/demos/images/nature-3.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img className='sliderimage' src="https://swiperjs.com/demos/images/nature-4.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img className='sliderimage' src="https://swiperjs.com/demos/images/nature-5.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img className='sliderimage' src="https://swiperjs.com/demos/images/nature-6.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img className='sliderimage' src="https://swiperjs.com/demos/images/nature-7.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img className='sliderimage' src="https://swiperjs.com/demos/images/nature-8.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img className='sliderimage' src="https://swiperjs.com/demos/images/nature-9.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img className='sliderimage' src="https://swiperjs.com/demos/images/nature-10.jpg" />
+              </SwiperSlide>
+            </Swiper>
+            <Swiper
+              onSwiper={setThumbsSwiper}
+              loop={true}
+              spaceBetween={10}
+              slidesPerView={4}
+              freeMode={true}
+              watchSlidesProgress={true}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className="mySwiper"
+            >
+              <SwiperSlide className='ac'>
+              <iframe width="130" height="150" src="https://www.youtube.com/embed/FCe1vPuB954?si=Ji0lDq6o_STvlgCB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+              </SwiperSlide>
+            </Swiper>
+          </>
             </Col>
-            <Col>
+            
+            <Col lg='6'>
                 <Col lg="8" className="mb-3">
                     <h3 className='productheading'>Cisco SG110 8 Port Gigabite Ethernet Switch</h3>
                     <h3 className='productheading'>SG110D-08-UK</h3>
                 </Col>
-                <Col lg="8" className="mt-4">
+                <Col lg="8" className="mt-5 mb-3">
                     <p className='productprice'>US $35.99</p>
                 </Col>
                 <Row>
@@ -39,8 +137,10 @@ function SingleProduct() {
                             <Col lg="5">
                                 <p className='productQuantity'>Quantity:</p>
                             </Col>
-                            <Col lg="2" className='quantitybox'>
-                                
+                            <Col lg="4">
+                                <Form.Group as={Col} controlId="formGridquantitybox" className='quantitybox'>
+                                    <Form.Control />
+                                </Form.Group>
                             </Col>
                         </Row>
                     </Col>
@@ -48,7 +148,7 @@ function SingleProduct() {
                         <p className='availablesold mt-1'>28 available / 18 sold</p>
                     </Col>
                 </Row>
-                <Col>
+                <Col className='mt-4'>
                     <div className="mb-2">
                         <Button className="buynowbtn" variant="primary" size="lg">
                         Buy Now 
@@ -56,45 +156,20 @@ function SingleProduct() {
                     </div>
                 </Col>
                 <Col>
-                    <div className="mb-2">
+                    <div className="mb-2 mt-2">
                         <Button className="addtocartbtn" variant="primary" size="lg">
                         Add to Cart 
                         </Button>{' '}
                     </div>
                 </Col>
             </Col>
-      </Row>
-       
-      <Row>
-        <Col>
-            <Accordion defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>Specification</Accordion.Header>
-                    <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>Accordion Item #2</Accordion.Header>
-                    <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
-        </Col>
-      </Row>
+        </Row>
+
+        {/* <Row fluid="md" className='mt-5'>
+                <Row>
+                    <Col>1 of 1</Col>
+                </Row>
+        </Row> */}
     </Container> 
   )
 }
